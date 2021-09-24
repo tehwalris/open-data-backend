c6789aef72848a5743b209a7752cd9fc23a2c68c
Philippe Voinov <philippevoinov@gmail.com>
2021-09-24 20:42:22 +0200

    Switch to python

--- shell.nix
+++ shell.nix
@@ -1,8 +1,20 @@
 { pkgs ? import <nixpkgs> {} }:
 with pkgs;
-mkShell {
-  nativeBuildInputs = [
-    nodejs
-    yarn
+let
+  my-python-packages = python-packages: with python-packages; [
+    numpy
+    pandas
+    matplotlib
+    tqdm
+    scikitlearn
+    jupyterlab
+    seaborn
   ];
-}
+in 
+  mkShell {
+    nativeBuildInputs = [
+      nodejs
+      yarn
+      (python3.withPackages my-python-packages)
+    ];
+  }
---
+++ src/hello.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print("hello world", np.array([1, 2, 3]))
