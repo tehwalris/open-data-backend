c7c5475675ae2a4a1c3710960ac8811fa2fdb1b8
Philippe Voinov <philippevoinov@gmail.com>
2021-09-25 21:28:16 +0200

    Hack smoother smoothing

--- src/smooth.py
+++ src/smooth.py
@@ -1,6 +1,14 @@
+import numpy as np
+
 def smooth(df, dim, window_size):
+  def stuff(s, q):
+    s = s.rolling(window_size).quantile(q)
+    s[s.diff() == 0] = np.nan
+    s = s.interpolate('cubic')
+    return s
+
   df = df.copy()
-  df[f'{dim}_high'] = df[dim].rolling(window_size, center=True).max()
-  df[f'{dim}_low'] = df[dim].rolling(window_size, center=True).min()
+  df[f'{dim}_high'] = stuff(df[dim], 0.9)
+  df[f'{dim}_low'] = stuff(df[dim], 0.1)
   df[dim] = df[dim].rolling(window_size, center=True).mean()
   return df
