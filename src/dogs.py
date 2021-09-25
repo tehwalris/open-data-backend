c9ca0281f850a7f2a531f0ee294703129bce3f8e
Marc Bitterli <nxt191@gmail.com>
2021-09-25 16:01:31 +0200

    dog questions

--- src/dogs.py
+++ src/dogs.py
@@ -4,7 +4,7 @@ import json
 
 from .path import get_path_from_root
 from .memoize import memoize
-from .response import response_from_df
+from .response import json_from_df, response_from_df
 
 def _load_data():
   df = pd.read_csv(
@@ -21,10 +21,69 @@ load_data = memoize(_load_data)
 def dog_gender():
   df = load_data()
   df = df["GESCHLECHT_HUND"].value_counts()
-  print("asdf")
-
 
   df = df.reset_index()
-  df.columns =["label","value"]
-  print(df)
+  df.columns =["x","y"]
+  return response_from_df(df)
+
+def owner_gender():
+  df = load_data()
+  df = df["GESCHLECHT"].value_counts()
+
+  df = df.reset_index()
+  df.columns =["x","y"]
+  return response_from_df(df)
+
+def dog_breed():
+  df = load_data()
+  temp = df["RASSE1"].value_counts().sort_values().to_frame()
+  size = temp.size
+  others = (df["RASSE1"].value_counts().tail(size-10).sum())
+  print(size,others)
+  temp = df["RASSE1"].value_counts().head(10)
+
+  temp["Andere"] = others
+  temp = temp.to_frame()
+  print(temp)
+  temp = temp.reset_index()
+  temp.columns =["x","y"]
+
+  return response_from_df(temp)
+
+def dogs_by_kreis():
+  df = load_data()
+  df = df["STADTKREIS"].value_counts().sort_index()
+  df[0] = df.sum()
+  df = df.to_frame()
+  df = df.reset_index()
+  df.columns =["placeId","value"]
+
+  return {
+      "unit": "dogs",
+      "values": json.loads(json_from_df(df)),
+    }
+
+def owner_age():
+  df = load_data()
+  df = df["ALTER"].value_counts().sort_index().to_frame()
+
+  df = df.reset_index()
+  df.columns =["x","y"]
+  return response_from_df(df)
+
+def dog_age():
+  df = load_data()
+  df = df["GEBURTSJAHR_HUND"].value_counts()
+  df = df[df>1].sort_index()
+
+  df = df.reset_index()
+  df.columns =["x","y"]
+  return response_from_df(df)
+
+def max_dog_count():
+  df = load_data()
+  df = df["HALTER_ID"].value_counts().head(1).to_frame()
+
+  df = df.reset_index()
+  df.columns =["x","y"]
   return response_from_df(df)
