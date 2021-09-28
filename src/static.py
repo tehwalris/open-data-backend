be32a2a93f77751cb8c7e1a362b9a08cdbf3c92d
Philippe Voinov <philippevoinov@gmail.com>
2021-09-28 21:53:55 +0200

    WIP: static search

--- src/static.py
+++ src/static.py
@@ -20,22 +20,22 @@ shutil.rmtree(str(out_folder), ignore_errors=True)
 out_folder.mkdir()
 (out_folder / "answer").mkdir()
 
-# def get_url(relative_path):
-#   return urllib.parse.urljoin(args.server, relative_path)
+def get_url(relative_path):
+  return urllib.parse.urljoin(args.server, relative_path)
 
-# def get_and_save(path: str):
-#   assert not path.startswith('/')
-#   assert not path.startswith('.')
+def get_and_save(path: str):
+  assert not path.startswith('/')
+  assert not path.startswith('.')
 
-#   print('saving', path)
-#   r = requests.get(get_url('/' + path))
-#   r.raise_for_status()
-#   (out_folder / path).write_bytes(r.content)
+  print('saving', path)
+  r = requests.get(get_url('/' + path))
+  r.raise_for_status()
+  (out_folder / path).write_bytes(r.content)
 
-# get_and_save('questions')
+get_and_save('questions')
 
-# for q in questions:
-#   get_and_save(f"answer/{q['id']}")
+for q in questions:
+  get_and_save(f"answer/{q['id']}")
 
 (out_folder / "search").mkdir()
 prepare_static_search(out_folder / "search")
